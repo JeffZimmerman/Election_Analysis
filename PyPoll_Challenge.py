a883511c07a45e958784472e5e6b7d4d15c118eb
Jeff Zimmerman <91562577+elchubasco@users.noreply.github.com>
2021-11-07 21:43:36 -0500

    Add files via upload

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -126,32 +126,31 @@ with open(file_to_save, "w") as txt_file:
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
 
-    # Retrieve vote count and percentage
+        # Retrieve vote count and percentage
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
-        candidate_results = (
-        f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
         
         # Print each candidate's voter count and percentage to the
         # terminal.
-    print(candidate_results)
+        print(candidate_results)
         #  Save the candidate results to our text file.
-    txt_file.write(candidate_results)
+        txt_file.write(candidate_results)
 
-    # Determine winning vote count, winning percentage, and candidate.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
+        # Determine winning vote count, winning percentage, and candidate.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
 
     # Print the winning candidate (to terminal)
-winning_candidate_summary = (
+        winning_candidate_summary = (
         f"-------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
-print(winning_candidate_summary)
+    print(winning_candidate_summary)
 
     # Save the winning candidate's name to the text file
-txt_file.write(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
